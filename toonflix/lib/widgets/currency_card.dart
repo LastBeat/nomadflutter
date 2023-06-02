51af03a02e28049367511eeabecee54238396208
LastBeat <devmon0927@gmail.com>
2023-06-02 23:00:32 +0900

    Code Challenge
    - Wrap Container in currency_card.dart
    - Add 'option' properties for Offset 'dy' value
    - Update main.dart

--- toonflix/lib/widgets/currency_card.dart
+++ toonflix/lib/widgets/currency_card.dart
@@ -5,6 +5,9 @@ class CurrencyCard extends StatelessWidget {
   final IconData icon;
   final bool isInverted;
 
+  // option proerties
+  final double option;
+
   final _blackColor = const Color(0xFF1F2123);
 
   const CurrencyCard(
@@ -13,71 +16,82 @@ class CurrencyCard extends StatelessWidget {
       required this.code,
       required this.amount,
       required this.icon,
-      required this.isInverted});
+      required this.isInverted,
+      required this.option});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        color: isInverted ? Colors.white : _blackColor,
-        borderRadius: BorderRadius.circular(25),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
-                      color: isInverted ? _blackColor : Colors.white,
-                      fontSize: 32,
-                      fontWeight: FontWeight.w600),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      amount,
-                      style: TextStyle(
+    return Transform.translate(
+      /*
+       Offset Logic
+       if option == 1 -> dy == 0
+       if option == 2 -> dy == -20
+       if option == 3 -> dy == -40
+       */
+
+      offset: Offset(0, (option - 1) * -20),
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          color: isInverted ? Colors.white : _blackColor,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
                         color: isInverted ? _blackColor : Colors.white,
-                        fontSize: 20,
+                        fontSize: 32,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        amount,
+                        style: TextStyle(
+                          color: isInverted ? _blackColor : Colors.white,
+                          fontSize: 20,
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      code,
-                      style: TextStyle(
-                        color: isInverted
-                            ? _blackColor
-                            : Colors.white.withOpacity(0.8),
-                        fontSize: 20,
+                      const SizedBox(
+                        width: 5,
                       ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-            Transform.scale(
-              scale: 2.2,
-              child: Transform.translate(
-                offset: const Offset(-5, 12),
-                child: Icon(
-                  icon,
-                  color: isInverted ? _blackColor : Colors.white,
-                  size: 88,
+                      Text(
+                        code,
+                        style: TextStyle(
+                          color: isInverted
+                              ? _blackColor
+                              : Colors.white.withOpacity(0.8),
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Transform.scale(
+                scale: 2.2,
+                child: Transform.translate(
+                  offset: const Offset(-5, 12),
+                  child: Icon(
+                    icon,
+                    color: isInverted ? _blackColor : Colors.white,
+                    size: 88,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
